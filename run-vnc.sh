9fab7eb482b86eebf7ec9e1d862164c0a12c9345
Trevor Dolby <trevor.dolby@ibm.com>
2022-11-28 20:44:21 +0000

    Add script and toolkit dependencies
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

---
+++ run-vnc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+# Allow the re-run of the profile to succeed to fix LD_LIBRARY_PATH issue 
+unset PROSPECTIVE_MQSI_BASE_FILEPATH
+
+# Start the X server itself
+vncserver -geometry 1600x1200
+
+# Start the port forwarder on port 6080
+/usr/share/novnc/utils/launch.sh --vnc localhost:5901 &
